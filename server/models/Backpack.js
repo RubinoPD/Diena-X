a67625760958e50fb19c28636bc9b43499d1ff9f
RubinoPD <robertaskask@gmail.com>
2025-04-06 18:05:47 +0300

    Add Backpack Schema

---
+++ server/models/Backpack.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const backpackSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Backpack = mongoose.model("Backpack", backpackSchema);
+module.exports = Backpack;
