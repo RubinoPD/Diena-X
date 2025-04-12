6ce7ccdcbcfa44d543ed85a1098ad6695694734b
RubinoPD <robertaskask@gmail.com>
2025-04-12 17:14:54 +0300

    added auth to services

---
+++ client/src/services/auth.js
@@ -0,0 +1,38 @@
+import api from "./api";
+
+export const register = async (userData) => {
+  try {
+    const response = await api.post("/auth/register", userData);
+    if (response.data.token) {
+      localStorage.setItem("token", response.data.token);
+    }
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || { message: "Ivyko klaida" };
+  }
+};
+
+export const login = async (credentials) => {
+  try {
+    const response = await api.post("/auth/login", credentials);
+    if (response.data.token) {
+      localStorage.setItem("token", response.data.token);
+    }
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || { message: "Ivyko klaida" };
+  }
+};
+
+export const logout = () => {
+  localStorage.removeItem("token");
+};
+
+export const getCurrentUser = async () => {
+  try {
+    const response = await api.get("/auth/me");
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || { message: "Ivyko klaida" };
+  }
+};
